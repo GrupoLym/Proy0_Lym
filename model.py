263af4824495d95e3557368560bd4e51c03f45b0
Sreys54 <s.reyesc2@uniandes.edu.co>
2023-08-29 20:07:04 -0500

    vamos que si podemos

--- model.py
+++ model.py
@@ -1,4 +1,15 @@
 
-palabras_clave = ["defVar", "drop", "letGo", "walk", "leap", "turn",
-                  "turnto", "get", "grab", "nop", "jump"]
 
+
+
+palabras_clave = ["defVar", "drop", "letGo", "walk", "leap", "turn",
+                  "turnto", "get", "grab", "nop", "jump", "Defproc"]
+
+def palabras_clave (palabras_clave, palabra):
+    for word in palabras_clave:
+        if palabra not in palabras_clave:
+            return no 
+
+#def palabra_parametro (palabras_clave, parametro, palabra):
+    #if palabra in palabras_clave:
+        #while parametro == 
