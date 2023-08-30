3608a0a94faa2203c4a8aafa52495e4c47f42ad7
Sreys54 <s.reyesc2@uniandes.edu.co>
2023-08-30 09:26:45 -0500

    un pequeño paso para el mundo un gran paso para mi

--- model.py
+++ model.py
@@ -14,12 +14,58 @@ parametros_get = ["1","2","3","4","5","6","7","8","9"]
 parametros_grab = ["1","2","3","4","5","6","7","8","9"]
 parametros_letGo = ["1","2","3","4","5","6","7","8","9"]
 
+def parametro_x_palabra (palabra):
+    if palabra == "jump":
+        return parametros_jump
+    elif palabra == "walk":
+        return parametros_walk
+    elif palabra == "leap":
+        return parametros_leap
+    elif palabra == "turn":
+        return parametros_turn
+    elif palabra == "turnto":
+        return parametros_turnto
+    elif palabra == "drop":
+        return parametros_drop
+    elif palabra == "get":
+        return parametros_get
+    elif palabra == "grab":
+        return parametros_grab
+    elif palabra == "letGo":
+        return parametros_letGo
+    else:
+        return ""
+
+""" No se si esta función sirva y sea la manera optimizada de la función de arriba,
+creo que no sirve porque el return va a terminar siendo un String igual a parametros_...
+más no la variable"""
+
+def parametro_x_palabra_opti (palabra, palabras_clave):
+    for word in palabras_clave:
+        if palabra in palabras_clave and palabra == "parametros_"+palabra:
+            return "parametros_"+palabra
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 def palabras_clave (palabras_clave, palabra):
-    for palabra in palabras_clave:
+    for word in palabras_clave:
         if palabra not in palabras_clave:
-            return False
+            return "No"
 
-#def palabra_parametro (palabras_clave, parametro, palabra):
-    #if palabra in palabras_clave:
-        #while parametro == 
+def palabra_parametro (palabras_clave, parametro, palabra):
+    for word in palabras_clave:
+        if palabra in palabras_clave:
+            comparacion = palabra 
+            prueba = parametro_x_palabra(comparacion)
+            if prueba != "":
+                for valor in prueba:
+                    if parametro in prueba:
+                        return "Yes"
+                    else:
+                        return "No"
+            else: 
+                return "No"
+            
+
+                
+
