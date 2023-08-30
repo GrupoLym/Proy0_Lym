f0e56207e632f791ba7486687ee54de62b1851e1
Sreys54 <s.reyesc2@uniandes.edu.co>
2023-08-30 00:01:43 -0500

    todos los parametros

--- model.py
+++ model.py
@@ -5,7 +5,18 @@
 palabras_clave = ["defVar", "drop", "letGo", "walk", "leap", "turn",
                   "turnto", "get", "grab", "nop", "jump", "Defproc"]
 
-parametros = ["x,y", "1","2","3","4","5","6","7","8","9",""]
+parametros_jump = ["x","y"]
+parametos_walk = ["1","2","3","4","5","6","7","8","9","front","left","right","back",
+                    "north","south","west","east"]
+parametros_leap = ["1","2","3","4","5","6","7","8","9","front","left","right","back",
+                    "north","south","west","east"]
+parametros_turn = ["left","right","around"]
+parametros_turnto = ["north","south","east","west"]
+parametros_drop = ["1","2","3","4","5","6","7","8","9"]
+parametros_get = ["1","2","3","4","5","6","7","8","9"]
+parametros_grab = ["1","2","3","4","5","6","7","8","9"]
+parametros_letGo = ["1","2","3","4","5","6","7","8","9"]
+
 
 def palabras_clave (palabras_clave, palabra):
     for word in palabras_clave:
