05a5502fbf2abf87f30769dab4d9deabbfdd5847
Ignacio Chaparro <i.chaparro@uniandes.edu.co>
2023-09-01 20:59:21 -0500

    modificacion consola

--- view.py
+++ view.py
@@ -3,6 +3,9 @@ import model
 def comenzar_parse():
     file = input("Ingrese el nombre del archivo: ")
     lista_tokens = model.create_tokens(file)
-    
+    if model.analizador(lista_tokens) == None:
+        print("Tu programa está bien escrito")
+    elif model.analizador(lista_tokens) == False:
+        print("Hay un error en tu programa")
     
 comenzar_parse()
